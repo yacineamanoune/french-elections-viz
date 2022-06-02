6ae84dd2ba44074c1a0ea8096d383e92783181f2
yac1n3 <amanouneyacine@gmail.com>
2022-06-03 00:53:01 +0200

    Add idf map

--- app.py
+++ app.py
@@ -7,45 +7,50 @@ import pandas as pd
 import json
 import numpy as np
 
-df = pd.read_excel("data/departements_2ndturn_results.xlsx")
-print(df.head())
+df_departements = pd.read_excel("data/departements_2ndturn_results.xlsx")
+df_communes = pd.read_excel("data/cities_2ndturn_results.xlsx")
 
 f = open('data/departements.geojson')
 departements = json.load(f)
 
+f = open('data/COMMUNEidf_light.geojson')
+communes_idf = json.load(f)
+
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
-mytitle = dcc.Markdown(children='')
-mygraph = dcc.Graph(figure={})
-dropdown = dcc.Dropdown(options=['Départements'],
-                        value='Départements',  # initial value displayed when page first loads
+mytitle = dcc.Markdown(children='# Résultat du second tour des éléctions présidentielles françaises')
+graph_dep = dcc.Graph(figure={})
+graph_com = dcc.Graph(figure={})
+dropdown = dcc.Dropdown(options=['Gagnant','Abstentions'],
+                        value='Gagnant',
                         clearable=False)
 
 
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([mytitle], width=6)
-    ], justify='center'),
+    ], justify='center',style={'textAlign':'center'}),
     dbc.Row([
-        dbc.Col([mygraph], width=12)
+        dbc.Col([graph_dep], width=8),
+        dbc.Col([graph_com], width=4)
     ]),
     dbc.Row([
         dbc.Col([dropdown], width=6)
-    ], justify='center'),
+    ],justify='center'),
 
 ], fluid=True)
 
 
 @app.callback(
-    Output(mygraph, 'figure'),
-    Output(mytitle, 'children'),
+    Output(graph_dep, 'figure'),
+    Output(graph_com, 'figure'),
     Input(dropdown, 'value')
 )
 def update_graph(column_name):  
 
-    fig = px.choropleth_mapbox(df,
+    fig_dep = px.choropleth_mapbox(df_departements,
                     geojson=departements, 
-                    color="Gagnant",
+                    color=column_name,
                     mapbox_style='open-street-map',
                     locations='Code du département', 
                     featureidkey="properties.code",
@@ -53,10 +58,23 @@ def update_graph(column_name):
                     zoom = 4.5, 
                     opacity = 0.5
                    )
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig_dep.update_geos(fitbounds="locations", visible=False)
+    fig_dep.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-    return fig, '# '+column_name  
+    fig_com = px.choropleth_mapbox(df_communes, 
+                    geojson=communes_idf,
+                    color=column_name,
+                    mapbox_style='open-street-map',
+                    locations='Code', 
+                    featureidkey="properties.INSEE_COM",
+                    center={"lat": 48.8566, "lon": 2.5}, 
+                    zoom = 6.7, 
+                    opacity = 0.5
+                   )
+    fig_com.update_geos(fitbounds="locations", visible=False)
+    fig_com.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    return fig_dep,fig_com 
 
 
 # Run app
